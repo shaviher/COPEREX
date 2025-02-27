a58c3f6a9c350eef346c7886926246f33e8e639b
shaviher <javiherdeza@gmail.com>
2025-02-26 22:24:26 -0600

    feat(company):crear modelo de empresa

---
+++ configs/category.js
@@ -0,0 +1,12 @@
+export const category_allow = [
+    "Technology",
+    "Manufacturing",
+    "Services",
+    "Health and Wellness",
+    "Education",
+    "Food and Beverage",
+    "Retail and E-commerce",
+    "Energy and Environment",
+    "Finance and Insurance",
+    "Tourism and Hospitality",
+]
---
+++ src/company/company.model.js
@@ -0,0 +1,55 @@
+import { Schema, model } from 'mongoose';
+import { category_allow } from '../../configs/category';
+
+const companySchema = new Schema({
+    name:{
+        type: String,
+        required: [true, 'Company name is required'],
+        unique: true
+    },
+    description:{
+        type: String,
+        required: [true, 'Company description is required'],
+        maxLength: [200,  "The username cannot exceed 200 characters"]
+    },
+    category_allow:{
+        type: String,
+        enum: category_allow,
+        required: [true, 'Company category is required']
+    },
+    impactLevel:{
+        type: String,
+        required: [true, 'Company impact level is required'],
+        enum: ["High", "Medium", "Low"]
+    },
+    foundationYear:{
+        type: Number,
+        required: [true, 'Company foundation year is required'],
+        min: [1900, "The year cannot be less than 1900"],
+        max: new Date().getFullYear()
+    },
+    email:{
+        type: String,
+        required: [true, 'Company email is required'],
+        unique: true
+    },
+    phone:{
+        type: String,
+        required: [true, 'Company phone is required'],
+        unique: true
+    },
+    address:{
+        type: String,
+        required: [true, 'Company address is required']
+    },
+    website:{
+        type: String,
+        required: [true, 'Company website is required']
+    },
+    admin: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+export const Company = model('Company', companySchema)
