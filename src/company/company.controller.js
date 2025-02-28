771eb0b51c0cd96e47f304e54305a8d1c5131bc6
shavi <javiherdeza@gmail.com>
2025-02-28 10:18:43 -0600

    feat(company):agregar método actualizar

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -45,3 +45,54 @@ export const registerCompany = async (req, res) => {
         })
     }
 }
+
+export const updateCompany = async (req, res) => {
+    try{
+        const { cid } = req.params 
+        const data = req.body
+        const adminID = req.user.id
+
+        const admin = await User.findById(adminID)
+
+        if (!admin || admin.rol !== "ADMIN") {
+            return res.status(403).json({
+                success: false,
+                message: "Only administrats can update company",
+                error: "Access denied"
+            })
+        }
+
+        const company = await Company.findByIdAndUpdate(cid, data, { new: true })
+
+        if(!company) {
+            return res.status(404).json({
+                success: false,
+                message: "Company not found",
+                error: err.message
+            })
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Company has been updated",
+            company: {
+                name: company.name,
+                description: company.description,
+                category: company.category,
+                impactLevel: company.impactLevel,
+                foundationYear: company.foundationYear,
+                email: company.email,
+                phone: company.phone,
+                address: company.address,
+                website: company.website
+            }
+        })
+
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Failed to update Company",
+            error: err.message
+        })
+    }
+}
--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express"
-import { registerCompany } from "./company.controller.js"
-import { registerCompanyValidator } from "../middlewares/validar-company.js"
+import { registerCompany, updateCompany } from "./company.controller.js"
+import { registerCompanyValidator, updateCompanyValidator } from "../middlewares/validar-company.js"
 
 
 const router = Router()
 
 router.post("/register", registerCompanyValidator, registerCompany)
+
+router.put("/update/:cid", updateCompanyValidator, updateCompany)
 
 export default router
