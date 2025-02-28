ef83b02a4206334905952d0b772bafb973511028
shavi <javiherdeza@gmail.com>
2025-02-28 10:51:10 -0600

    feat(company): listar y exportar empresas

--- src/company/company.controller.js
+++ src/company/company.controller.js
@@ -1,5 +1,8 @@
 import Company from "./company.model.js"
 import User from "../user/user.model.js"
+import ExcelJS from "exceljs"
+import fs from 'fs';
+import path from 'path';
 
 
 export const registerCompany = async (req, res) => {
@@ -92,6 +95,75 @@ export const updateCompany = async (req, res) => {
         return res.status(500).json({
             success: false,
             message: "Failed to update Company",
+            error: err.message
+        })
+    }
+}
+
+export const listCompany = async (req, res) => {
+    try{
+        const { sort, category, minYears, maxYears } = req.query
+        let sortOptions = {}
+        let queryFilters = {} 
+
+         // Filtros de categoría
+         if (category) {
+            queryFilters.category = category
+        }
+
+        // Filtro de años de trayectoria
+        if (minYears || maxYears) {
+            const currentYear = new Date().getFullYear()
+            const minFoundationYear = minYears ? currentYear - minYears : 1900
+            const maxFoundationYear = maxYears ? currentYear - maxYears : currentYear
+            queryFilters.foundationYear = { $gte: minFoundationYear, $lte: maxFoundationYear }
+        }
+
+        if(sort === "A-Z") sortOptions.name = 1
+        if(sort === "Z-A") sortOptions.name = -1
+
+        const company = await Company.find(queryFilters).sort(sortOptions)
+
+        const workbook = new ExcelJS.Workbook()
+        const worksheet = workbook.addWorksheet(`Company`)
+
+        worksheet.columns = [
+            { header: 'Name', key: 'name', width: 30 },
+            { header: 'Description', key: 'description', width: 30 },
+            { header: 'Category', key: 'category', width: 30 },
+            { header: 'Impact Level', key: 'impactLevel', width: 20 },
+            { header: 'Foundation Year', key: 'foundationYear', width: 20 },
+            { header: 'Email', key: 'email', width: 50 },
+            { header: `Phone`, key: `phone`, width: 50},
+            { header: `Address`, key: `address`, width: 50},
+            { header: `Website`, key: `website`, width: 50}
+        ]
+
+        company.forEach(company => {
+            worksheet.addRow(company)
+        })
+
+        const directoryPath  = path.join(path.resolve(), `excel`)
+        if(!fs.existsSync(directoryPath)) {
+            fs.mkdirSync(directoryPath)
+        }
+
+        const now = new Date()
+        const fecha = now.toISOString().replace(/T/, '_').replace(/:/g, '-').split('.')[0]
+        const filePath = path.join(directoryPath, `company_${fecha}.xlsx`)
+
+        await workbook.xlsx.writeFile(filePath)
+
+        return res.status(200).json({
+            success: true,
+            message: "company listed successfully",
+            company
+        });
+
+    }catch(err){
+        return res.status(500).json({
+            success: false,
+            message: "Failed to list company",
             error: err.message
         })
     }
--- src/company/company.routes.js
+++ src/company/company.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import { registerCompany, updateCompany } from "./company.controller.js"
+import { listCompany, registerCompany, updateCompany } from "./company.controller.js"
 import { registerCompanyValidator, updateCompanyValidator } from "../middlewares/validar-company.js"
 
 
@@ -8,5 +8,7 @@ const router = Router()
 router.post("/register", registerCompanyValidator, registerCompany)
 
 router.put("/update/:cid", updateCompanyValidator, updateCompany)
+
+router.get("/list-export", listCompany)
 
 export default router
